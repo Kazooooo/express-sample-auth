0585c220fedcf08371bbce53273b9709d93f4f26
Kazooooo <cest93@gmail.com>
2017-03-07 00:11:20 +0900

    fix action types to auth reducer

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,4 +1,7 @@
-import * as action from '../actions/types';
+import {
+  AUTH_USER,
+  UNAUTH_USER,
+} from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
